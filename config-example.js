f62bd663ae0f51dab30146d813ac2090252a758a
wenewzhang <wenewboy@gmail.com>
2019-02-21 11:12:49 +0800

    config example

---
+++ config-example.js
@@ -0,0 +1,23 @@
+// NOTE: please update this config file with your own
+module.exports = {
+  clientId: 'ed882a39-0b0c-4413-bbd9-221cdeee56bf',
+  clientSecret: '92fa342d88a7aa7017527cdc1cbb913cedef2708765e2c1e438ae041d9acc3df',
+  assetPin: '910147',
+  sessionId: '0be3419c-760b-4d45-bac7-6351d74f21f6',
+  aesKey: '5fQXss5/BACJsR+1R18XzgAcHsdJmK/dIybegfMqq1w',
+  privateKey: `-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQCjpYA6NMBzda/oTfUZPWCwUMv+orjpczvd6mu6QXS0NGEOJWYd
+5rxPR9uqpeAc8k45LgxpwAHgQw9sBinTyObFZJvLUlGmw3dKZkWum35wJmS9QrdP
+Uqa7UyQUcaI8Iwkgx9rhnBWlAhOrM9rsw7mMjjvtNeUe4lkeSQeTk5fMKQIDAQAB
+AoGASph8lQvK1lbFMs2mdjTmSDjsZExkofXnSkc7iBGiUyq1Oy+NFwBAwprs5SLr
+wAYr5iC5g9MEh+fJteIxWtrvZjzKVWdKES4t2iRF5Bhh7QjkbUud9dzZ35Sd2hzp
+u0/UC4j/dEFmU9Z9IkjgUPSVBYmJ/DQ5Kf00I20NdMz9nwECQQDRSCgYMG/s+CjI
+XF0xDBuuHR+r/OQibomYXj8UDflKTWwBRhYLbgMKKDfZe+JWB5Dpr3dvpjt3rEyr
+so50BThZAkEAyC1qsJ0fY91OMYJYiwtMIimjcyedDgwcJdfBFX7NNfBPZEP1ZKeo
+B01J3DnD/4LVYm6OKTFHR8v9FcrH6tm4UQJABsHRqZZGxtvsVSvGRoMhkamXEDKG
+GyZ+gtDoMHmTBvcg+ZmNtKjEqFtMElWRpT1xTFpB/PTdDqwdfZ2yQ8S3YQJAK6m0
+Zteu3CKOhsAOvi11qtH4RAr09/ZuQVbhV+Xzrvs+5fjo0s67GPSj0rSK6vXHw5On
+0JZeHOwCxTkZCEH7MQJBAJHiO4ozWGejeXPJFG/KQfnMtkfR4sQQYO0si3FQN6Es
+B+0+m2lVMLF/zat12R2WYgFBFkFEcpphvR7Dg6V1cfw=
+-----END RSA PRIVATE KEY-----`,
+};
