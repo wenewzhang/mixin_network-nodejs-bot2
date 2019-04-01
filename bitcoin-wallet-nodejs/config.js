4525b78c520eb18767d3392765941388670d7f6e
wenewzhang <wenewboy@gmail.com>
2019-04-01 11:24:15 +0800

    rsa key pair

---
+++ bitcoin-wallet-nodejs/config.js
@@ -0,0 +1,23 @@
+// NOTE: please update this config file with your own
+module.exports = {
+  clientId: '21042518-85c7-4903-bb19-f311813d1f51',
+  clientSecret: 'will-generate-later',
+  assetPin: 'will-generate-later',
+  sessionId: '6ca194a4-727f-4e5f-a348-3c62987536ba',
+  aesKey: 'will-generate-later',
+  privateKey: `-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgQC1P7QK7rK8lyX7X5t4A/reu7Q94xJkAllf1NPsW7zUdVgs/BCV
+f4RA6YK2prTpHHqXSCAzToEmou8R0xBMdnT/IQqijt0NzMpvrphiQrKrXEHrKrLm
+at6eZHYvLoGEPYjVq6RrgLkt2Mjld+RfHWd4zHXeqSCVfHAH67+gcPHYCwIDAQAB
+AoGADGotoeYRthtATcSRuJnFMEZ5JRgNpW4HwymnznPGLmNPQ92MIUFXxL555prq
+n2EFAKG/GuSQsh3M9FKZtjMS9l0aXpXy4T4ieBptkhahKbGVMLbQBru8wo/Pow3r
+r+DuNJs64ELvBYyydS7r1Fm/mtrd38Aq+4+04Z3UDW50AUkCQQDuhy8FoA3TKdZM
+mIEiPFb2dW8ohe8MsGM370S8HFPk7kdCaarJbiJCx1E+KjUxbkAeEFcLqKgaALTu
+IVCikIHNAkEAwoZvPaY0yFB1+V8HuToIR4X7AqWMy6WTBZ9U4wp34aNO21DLcrqf
+P40FHrHvqbWNK5bS8nSxLiv0kYL6+ezJNwJAO/GxOYKttsGu33T8DvSHDk0Y8GAo
+YVH6vVXeOkAMPV48fk47439QEOQyYKMO1ytT5bpJhd6O0GoZDjdFInWaiQJAAq4l
+hDzxBz2MkpYLnjK9gHbJIZ00Vm3+m5o5ajNvuW0tnfn8A6WsogyIYIblHXqB6nLW
+jz6qXk9+vC6I1L69ewJAasE+oC3TMblSOC9xqeBQgm8BPhb0UwJL4UuZLOSyUETr
++bAwyiZ37Cc7r/nxKhVH+FwMCVoeNUMcRIYYMRjwmg==
+-----END RSA PRIVATE KEY-----`,
+};
