e568255e63df4fb43395052bce83f03c2615955d
wenewzhang <wenewboy@gmail.com>
2019-04-01 16:42:47 +0800

    get pem privatekey & publickey;publicKey to session key

--- bitcoin-wallet-nodejs/bitcoin-wallet-nodejs.js
+++ bitcoin-wallet-nodejs/bitcoin-wallet-nodejs.js
@@ -1,16 +1,22 @@
-const crypto = require('crypto');
-
-const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-  modulusLength: 2048,
+const { generateKeyPairSync } = require('crypto');
+const { publicKey, privateKey } = generateKeyPairSync('rsa',
+{   modulusLength: 1024,  // the length of your key in bits
+    publicKeyEncoding: {
+      type: 'spki',       // recommended to be 'spki' by the Node.js docs
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs1',      // recommended to be 'pkcs8' by the Node.js docs
+      format: 'pem',
+      //cipher: 'aes-256-cbc',   // *optional*
+      //passphrase: 'top secret' // *optional*
+  }
 });
+publicKey1 = publicKey.replace("-----BEGIN PUBLIC KEY-----","");
+publicKey2 = publicKey1.replace("-----END PUBLIC KEY-----","");
+publicKey3 = publicKey2.replace(/\r?\n|\r/g, "");
 
-const sign = crypto.createSign('SHA256');
-sign.update('some data to sign');
-sign.end();
-const signature = sign.sign(privateKey);
+console.log(publicKey);
+console.log(publicKey3);
 
-const verify = crypto.createVerify('SHA256');
-verify.update('some data to sign');
-verify.end();
-console.log(verify.verify(publicKey, signature));
-console.log(privateKey);
+// console.log(sign.sign(privateKey, 'base64'));
