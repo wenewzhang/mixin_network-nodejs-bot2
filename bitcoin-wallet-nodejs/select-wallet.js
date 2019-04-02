dcf05ad80dd9f16106a7512e1870303f5c9f4c13
wenewzhang <wenewboy@gmail.com>
2019-04-02 21:22:44 +0800

    console gui but await doesn't work after fork

--- bitcoin-wallet-nodejs/select-wallet.js
+++ bitcoin-wallet-nodejs/select-wallet.js
@@ -1,46 +1,148 @@
+const childProcess     =    require('child_process');
+const path             =    require('path');
 const fs               =    require('fs');
+const inquirer         =    require('inquirer');
+const { HttpClient }   =    require('mixin-node-client');
+const config           =    require('./config');
+const pem              =    require('pem-file');
 const csv              =    require("fast-csv");
-var parse = require('csv-parse');
-var async = require('async');
+const { oaepDecrypt }  =    require('./crypto');
+const client           =    new HttpClient(config);
+var Promise = require('promise');
+var scriptName         =    path.basename(__filename);
 
-var stream = fs.createReadStream("mybitcoin_wallet.csv");
-let walletList = [];
-let firstLine  = '';
-csv
- .fromStream(stream, {headers: false})
- .on("data", function(data){
-     console.log(data);
-     console.log("line end\n",data[3]);
-     walletList.push(data[3]);
-     console.log(walletList);
-     if (firstLine === '') { firstLine = data[3];}
- })
- .on("end", function(){
-     console.log("done");
-     var PromptMsg;
-     PromptMsg  = "1: Create Wallet\n2: Read Bitcoin balance & Address \n3: Read USDT balance & Address\n4: Read EOS balance & address\n";
-     PromptMsg += "5: pay 0.0001 BTC buy USDT\n6: Read ExinCore Price(USDT)\n7: Read ExinCore Price(BTC)\n";
-     PromptMsg += "8: pay 1 USDT buy BTC\n9: Read Snapshots\na: Verify bot PIN code\nv: Verify wallet PIN code\n";
-     PromptMsg += "q: Exit \nMake your choose:";
-     const inquirer = require('inquirer');
+'use strict';
 
-     // const TYPE_CREATE_WALLET             = '1: Create Wallet';
-     // const TYPE_BITCOIN_INFO              = '2: Read Bitcoin balance & Address ';
-     // const TYPE_USDT_INFO                 = '3: Read USDT balance & Address';
-     // const TYPE_EOS_INFO                  = '4: Read EOS balance & Address';
+function runScript(scriptPath, args, callback) {
 
-     const prompts = [
-       {
-         name: 'type',
-         type: 'list',
-         default: firstLine,
-         message: PromptMsg,
-         choices: walletList,
-       },
-     ];
-     (async () => {
-       const args = await inquirer.prompt(prompts);
-       console.log('dappCreate.args', args);
-     })();
- });
-console.log("-----LINE END OF------");
+    // keep track of whether callback has been invoked to prevent multiple invocations
+   new Promise((resolve, reject) => {
+     var invoked = false;
+     const spawnOptions = {
+       // cwd: process.cwd(),
+       detached: true,
+       stdio: 'inherit',
+     };
+    var process = childProcess.fork(scriptPath, args, spawnOptions);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+    });
+}
+
+// Now we can run a script and invoke a callback when complete, e.g.
+if ( process.argv.length == 2 ) {
+
+  var stream = fs.createReadStream("mybitcoin_wallet.csv");
+  let walletList = [];
+  walletList.push("Create Wallet");
+  let firstLine  = '';
+  csv
+   .fromStream(stream, {headers: false})
+   .on("data", function(data){
+       walletList.push(data[3]);
+       if (firstLine === '') { firstLine = data[3];}
+   })
+   .on("end", function(){
+       var PromptMsg;
+       PromptMsg = "\nMake your choose(select the uuid for open the specified wallet):";
+       const prompts = [
+         {
+           name: 'type',
+           type: 'list',
+           default: firstLine,
+           message: PromptMsg,
+           choices: walletList,
+         },
+       ];
+       (async () => {
+         walletList.push("Exit");
+         const args = await inquirer.prompt(prompts);
+         runScript(scriptName, [args.type], function (err) {
+             if (err) throw err;
+         });
+       })();
+   });
+}
+if ( process.argv.length == 3 ) {
+  console.log(' args: ' + (process.argv[2]));
+  if ( process.argv[2] === "Exit") { process.exit();}
+  if ( process.argv[2] === "Create Wallet") {
+    console.log("create wallet ....");
+
+      const { generateKeyPairSync } = require('crypto');
+      const { publicKey, privateKey } = generateKeyPairSync('rsa',
+      {   modulusLength: 1024,  // the length of your key in bits
+          publicKeyEncoding: {
+            type: 'spki',       // recommended to be 'spki' by the Node.js docs
+            format: 'pem'
+          },
+          privateKeyEncoding: {
+            type: 'pkcs1',      // recommended to be 'pkcs8' by the Node.js docs
+            format: 'pem',
+            //cipher: 'aes-256-cbc',   // *optional*
+            //passphrase: 'top secret' // *optional*
+        }
+      });
+      publicKey1 = publicKey.replace("-----BEGIN PUBLIC KEY-----","");
+      publicKey2 = publicKey1.replace("-----END PUBLIC KEY-----","");
+      publicKey3 = publicKey2.replace(/\r?\n|\r/g, "");
+
+      console.log(publicKey);
+      console.log(publicKey3);
+      (async () => {
+      const info = await client.createUser({full_name : "nodejs bitcoin wallet",
+                                            session_secret: publicKey3,
+                                          });
+      let aesKey = '';
+      const privateKeyBytes = pem.decode(Buffer.from(privateKey));
+      const aesKeyBuffer = await oaepDecrypt(
+        Buffer.from(info.pin_token, 'base64'),
+        privateKeyBytes,
+        'SHA-256',
+        Buffer.from(info.session_id)
+      );
+      aesKey = Buffer.from(aesKeyBuffer).toString('base64');
+      console.log(aesKey);
+
+      var csvStream = csv.createWriteStream({headers: false, ignoreEmpty: true}),
+          writableStream = fs.createWriteStream("./mybitcoin_wallet.csv", {flags: 'a'});
+
+      writableStream.on("finish", function(){
+        console.log("Bitcoin wallet DONE!");
+      });
+      csvStream.pipe(writableStream);
+      csvStream.write({a: privateKey, b: info.pin_token,
+                       c: info.session_id, d: info.user_id,
+                       e: "123456"}
+                     );
+      csvStream.end();
+      fs.appendFile("./mybitcoin_wallet.csv", require("os").EOL, function(){});
+
+      const newUserConfig = {clientId: info.user_id, aesKey: aesKey,
+                             privateKey: privateKey, sessionId: info.session_id,
+                             clientSecret: "do not need", assetPin: "123456"};
+      console.log(newUserConfig);
+      const newUserClient = new HttpClient(newUserConfig);
+      var info2 = await newUserClient.updatePin({oldPin : "",
+                                                 newPin: "123456",
+                                               });
+      console.log(info2);
+
+      const verifyPin = await newUserClient.verifyPin("123456");
+      console.log({ verifyPin });
+    });
+  }
+}
