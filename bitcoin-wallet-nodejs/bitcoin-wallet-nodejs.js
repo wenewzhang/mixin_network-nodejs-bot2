d51f656d4da7230154d5daddb596a0cf362fb71e
wenewzhang <wenewboy@gmail.com>
2019-04-02 10:41:01 +0800

    Bitcoin Wallet Write to CSV

--- bitcoin-wallet-nodejs/bitcoin-wallet-nodejs.js
+++ bitcoin-wallet-nodejs/bitcoin-wallet-nodejs.js
@@ -1,4 +1,3 @@
-// const { SocketClient, isMessageType } = require('mixin-node-client');
 const { HttpClient } = require('mixin-node-client');
 const config = require('./config');
 const client = new HttpClient(config);
@@ -26,14 +25,35 @@ const client = new HttpClient(config);
 
   // const assets = await client.getUserAssets();
   // console.log(assets);
-  console.log('Supported Endpoints by HttpClient', client.getEndpoints());
-  const user = await client.getUser("21042518-85c7-4903-bb19-f311813d1f51");
-  console.log(user);
-  console.log(new Date().toString());
 
+  // console.log('Supported Endpoints by HttpClient', client.getEndpoints());
+  // const user = await client.getUser("21042518-85c7-4903-bb19-f311813d1f51");
+  // console.log(user);
+  // console.log(new Date().toString());
+  //
   const info = await client.createUser({full_name : "bitcoin wallet",
                      session_secret: publicKey3,
                    });
   console.log(info);
+  console.log(privateKey);
+  console.log(info.pin_token);
+  console.log(info.session_id);
+  console.log(info.user_id);
+  // var info2 = await client.updatePin({oldPin : "",
+  //                                     newPin: "123456",
+  //                                    });
+  // console.log(info2);
+  const fs = require('fs');
+  var csv = require("fast-csv");
+  var csvStream = csv.createWriteStream({headers: false}),
+      writableStream = fs.createWriteStream("./mybitcoin_wallet.csv");
+
+  writableStream.on("finish", function(){
+    console.log("Bitcoin wallet DONE!");
+  });
+
+  csvStream.pipe(writableStream);
+  csvStream.write({a: privateKey, b: info.pin_token, c: info.session_id, d: info.user_id, e: "123456"});
+  csvStream.end();
 })();
 // console.log(sign.sign(privateKey, 'base64'));
